f7ce53a1b2ecfb1939b73ad3134872c0b4995d5a
emmafewer <emmafewer20@gmail.com>
2020-12-17 17:46:22 -0800

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :non_clickbait
+
+    def non_clickbait
+        array = ["Won't Believe","Secret","Top [number]","Guess"]
+        if self.title == nil || !array.any? {|phrase| self.title.include? phrase}
+            errors.add(:title, "is invalid if not clickbait")
+        end
+    end
 end
